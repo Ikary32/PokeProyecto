4135de3e61d6c5f7bf8d66098a7ae165b6f5e109
Ikary32 <chillgaming98@gmail.com>
2025-03-25 21:38:17 -0500

    pokemondetail

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const pokemonlist = document.getElementById("pokemonList")
+const pokemonList = document.getElementById("pokemonList")
+const pokemonDetail = document.getElementById("pokemonDetail")
+const pokemonInfo = document.getElementById("pokemonInfo")
+const btnBack = document.getElementById("btnBack")
 
 async function getPokemonData(pokemonID){
     try {
@@ -16,18 +19,41 @@ function displayPokemon(pokemon){
     const pokemonCard = document.createElement("div")
     pokemonCard.classList.add("pokemon-card")
     pokemonCard.innerHTML = `
-    <img src="${pokemon.sprites.front_default}">
+    <img src="${pokemon.sprites.front_default}" alt="imagen del ${pokemon.name}">
     <h3>${pokemon.name}</>
     <p> ID: ${pokemon.id}</p>
     `
+    pokemonCard.addEventListener("click",()=>showPokemonDetail(pokemon))
+    pokemonList.appendChild(pokemonCard)
+    return true
+}
+function showPokemonDetail(pokemon){
+    pokemonList.style.display = "none"
+    pokemonDetail.style.display ="block"
+    document.body.classList.add('pokemon-detail-open');
+    let types = '';
+    for (let i = 0; i < pokemon.types.length; i++) {
+        types += pokemon.types[i].type.name;  
+        if (i < pokemon.types.length - 1) {
+            types += ', '; 
+        }
+    }
     
-    pokemonlist.appendChild(pokemonCard)
+    pokemonInfo.innerHTML =`
+    <img src="${pokemon.sprites.front_default}" alt="image view front ${pokemon.name}">
+    <img src="${pokemon.sprites.back_default}" alt="image view back ${pokemon.name}">
+    <p><strong>Types:</strong> ${types}</p>
+    `
 }
 async function loadPokedex() {
-    for (let i=1;i<100;i++){
+    for (let i=1;i<1500;i++){
     let pokemon = await getPokemonData(i)
     displayPokemon(pokemon)
-    console.log(pokemon)
 }
 }
+btnBack.addEventListener("click",()=>{
+    pokemonList.style.display = "grid"
+    pokemonDetail.style.display ="none"
+    document.body.classList.remove('pokemon-detail-open');
+})
 loadPokedex()
